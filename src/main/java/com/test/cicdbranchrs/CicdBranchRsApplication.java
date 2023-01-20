4c1c3f16ba73a627e519fe49969f173fd5849f34
jhm <jhmoon@howser.co.kr>
2023-01-20 17:02:38 +0900

    Initial commit

---
+++ src/main/java/com/test/cicdbranchrs/CicdBranchRsApplication.java
@@ -0,0 +1,13 @@
+package com.test.cicdbranchrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdBranchRsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CicdBranchRsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/test/cicdbranchrs/CicdBranchRsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.cicdbranchrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdBranchRsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
